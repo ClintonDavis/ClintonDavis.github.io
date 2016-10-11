ca13b02a464e7e810c1ae900655f1c8ad7db1592
Clinton Davis <clinton.davis.2007@gmail.com>
2016-10-11 08:27:55 -0500

    Healthier Buckeye Proposed Transit Routes FRS RFL

---
+++ HB/data/json_ptsshp1.js
@@ -0,0 +1,27 @@
+var json_ptsshp1={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "FRS Transportation", "StopNumber": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -83.5974924, 39.2027163 ] } },
+{ "type": "Feature", "properties": { "Name": "Rockhold Brown & Co Bank", "StopNumber": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -83.4835533, 39.1990906 ] } },
+{ "type": "Feature", "properties": { "Name": "The Joey's Pizza", "StopNumber": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -83.4347521, 39.2025838 ] } },
+{ "type": "Feature", "properties": { "Name": "Highland County Senior Center", "StopNumber": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -83.611345, 39.1967935 ] } },
+{ "type": "Feature", "properties": { "Name": "Merchants National Bank", "StopNumber": 5.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6115008, 39.2024121 ] } },
+{ "type": "Feature", "properties": { "Name": "Samaritan Outreach Services", "StopNumber": 6.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6079447, 39.2082691 ] } },
+{ "type": "Feature", "properties": { "Name": "Walgreens", "StopNumber": 7.0 }, "geometry": { "type": "Point", "coordinates": [ -83.60738, 39.219164 ] } },
+{ "type": "Feature", "properties": { "Name": "Highland District Hospital", "StopNumber": 8.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6097728, 39.2208787 ] } },
+{ "type": "Feature", "properties": { "Name": "Scioto Paint Valley Mental Health", "StopNumber": 9.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6105329, 39.2227229 ] } },
+{ "type": "Feature", "properties": { "Name": "FRS Group", "StopNumber": 10.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6112525, 39.2232773 ] } },
+{ "type": "Feature", "properties": { "Name": "Highland County Community Action Organization", "StopNumber": 11.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6115037, 39.2324835 ] } },
+{ "type": "Feature", "properties": { "Name": "Highland County Jobs and Family Services", "StopNumber": 12.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6083221, 39.2375846 ] } },
+{ "type": "Feature", "properties": { "Name": "Southern State Community College", "StopNumber": 13.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6134307, 39.23651 ] } },
+{ "type": "Feature", "properties": { "Name": "Highland County Homeless Shelter", "StopNumber": 14.0 }, "geometry": { "type": "Point", "coordinates": [ -83.617846, 39.236807 ] } },
+{ "type": "Feature", "properties": { "Name": "Cedar Woods", "StopNumber": 15.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6129561, 39.2282288 ] } },
+{ "type": "Feature", "properties": { "Name": "Kroger", "StopNumber": 16.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6194528, 39.2255071 ] } },
+{ "type": "Feature", "properties": { "Name": "Walmart", "StopNumber": 17.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6221334, 39.2282903 ] } },
+{ "type": "Feature", "properties": { "Name": "Treewood Apartments", "StopNumber": 18.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6138536, 39.2140346 ] } },
+{ "type": "Feature", "properties": { "Name": "Hi-Land Terrace Apartments", "StopNumber": 19.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6133806, 39.2114566 ] } },
+{ "type": "Feature", "properties": { "Name": "West Main Urgent Care", "StopNumber": 20.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6148708, 39.2026907 ] } },
+{ "type": "Feature", "properties": { "Name": "Community Markets", "StopNumber": 21.0 }, "geometry": { "type": "Point", "coordinates": [ -83.6152636, 39.202747 ] } }
+]
+}
